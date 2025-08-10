088ab92cf10c22c4e748a1e0323df0f27c5f8841
Sakura group <115202515+sakuradev01@users.noreply.github.com>
2025-08-11 06:56:33 +0700

    Update PrivateRoute.jsx

--- client/src/utils/PrivateRoute.jsx
+++ client/src/utils/PrivateRoute.jsx
@@ -1,3 +1,5 @@
+// File: client/src/utils/PrivateRoute.jsx
+
 import { useSelector } from "react-redux";
 import { Navigate } from "react-router-dom";
 import PropTypes from "prop-types";
@@ -6,9 +8,11 @@ const PrivateRoute = ({ children }) => {
   PrivateRoute.propTypes = {
     children: PropTypes.node.isRequired,
   };
-  const { isAuthenticated } = useSelector((state) => state.auth);
+  // Ganti `isAuthenticated` dengan `status`
+  const { status } = useSelector((state) => state.auth);
 
-  if (!isAuthenticated) {
+  // Periksa status autentikasi yang benar dari Redux store
+  if (!status) {
     return <Navigate to="/login" replace />;
   }
 
