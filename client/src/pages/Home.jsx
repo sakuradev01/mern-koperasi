0cdbed94b5a4d310e058ef8e0838833a12abb32e
Sakura group <115202515+sakuradev01@users.noreply.github.com>
2025-08-11 07:02:27 +0700

    Update Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,4 +1,23 @@
+// File: client/src/pages/Home.jsx
+
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+
 const Home = () => {
+  const navigate = useNavigate();
+  const { status } = useSelector((state) => state.auth);
+
+  useEffect(() => {
+    // Jika pengguna sudah login, arahkan ke dashboard
+    if (status) {
+      navigate("/dashboard", { replace: true });
+    } else {
+      // Jika pengguna belum login, arahkan ke halaman login
+      navigate("/login", { replace: true });
+    }
+  }, [status, navigate]);
+
   return <div className=" bg-gray-100 flex items-center justify-center"></div>;
 };
 
